c50fc048fac7f8ec681285306327a480ff694f89
Xadezign <xadezign@gmail.com>
2018-09-27 17:20:50 +0200

    middleware to Installed Apps + Migration

--- website/models.py
+++ website/models.py
@@ -2,8 +2,8 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
 class Page(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
